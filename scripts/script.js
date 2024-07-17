584ee1ad596f8ae4e0350242ec54c61c490b3601
Vico-Romero <vico.m.romero@gmail.com>
2024-07-17 01:52:26 -0600

    Arreglo boton add

--- scripts/script.js
+++ scripts/script.js
@@ -6,11 +6,13 @@ const formProfile = document.querySelector(".popup__input");
 const inputNameNode = formProfile.querySelector(".popup__text_title");
 const inputAboutNode = formProfile.querySelector(".popup__text_about");
 const closeProfilePopupButton = popupProfile.querySelector(".popup__close");
+const popupSaveButton = formProfile.querySelector(".popup__submit-btn");
 
 profileEditButton.addEventListener("click", function () {
   popupProfile.classList.add("active");
   inputNameNode.value = profileNameNode.textContent;
   inputAboutNode.value = profileAboutNode.textContent;
+  popupSaveButton.classList.remove("active");
 });
 
 formProfile.addEventListener("submit", function (event) {
@@ -18,10 +20,12 @@ formProfile.addEventListener("submit", function (event) {
   if (inputNameNode.value !== "" && inputAboutNode.value !== "") {
     profileNameNode.textContent = inputNameNode.value;
     profileAboutNode.textContent = inputAboutNode.value;
+    popupSaveButton.classList.add("active");
     popupProfile.classList.remove("active");
   }
 });
 
 closeProfilePopupButton.addEventListener("click", function () {
   popupProfile.classList.remove("active");
+  popupSaveButton.classList.remove("active");
 });
